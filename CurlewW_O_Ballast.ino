8251bb36c499802d8709dcc5da7dad7884617102
Craig MacFarlane <camacfarl@gmail.com>
2020-08-14 22:47:42 -0700

    Just a bit of style cleanup to make it easier to read.

--- CurlewW_O_Ballast.ino
+++ CurlewW_O_Ballast.ino
@@ -1,68 +1,58 @@
-//Project Curlew - Block 4//
+// Project Curlew - Block
 
-//Servo.Steering//
+// Servo.Steering
 #include <Servo.h>
 Servo servo1;
 const int ServopotPin = A1;
 int val;
-//Motor.Propulsion//
+
+// Motor Propulsion
 const int DIR_A = 5;
 const int DIR_B = 4;
 const int PWM = 6;
 const int switchPin = 3;
 const int MotorpotPin = A0;
 
-
-
-void setup() {
-  //Servo.Setup//
+void setup() 
+{
+  // Servo.Setup
   servo1.attach(9);
-  //Motor.Setup//
+
+  // Motor.Setup
   pinMode(DIR_A, OUTPUT);
   pinMode(DIR_B, OUTPUT);
   pinMode(PWM, OUTPUT);
   pinMode(switchPin, INPUT);
-
-
 }
 
-void loop()
-
-
-  //Servo.Ops//
-
- { 
+void loop() 
+{ 
+  // Servo Ops
   val = analogRead(ServopotPin) ;
-  val = map (val, 0, 1023, 0, 180);
-  servo1. write (val);
+  val = map(val, 0, 1023, 0, 180);
+  servo1.write(val);
   delay(20);
-  //Motor.Ops//
+
+  // Motor Ops
   int speed = analogRead(MotorpotPin) / 4;
 
-  if (digitalRead(switchPin) == HIGH)
-  {
+  if (digitalRead(switchPin) == HIGH) {
     forward(speed);
-  }
-  else
-  {
+  } else {
     reverse(speed);
   }
 }
+
 void forward(int spd)
 {
   digitalWrite(DIR_A, HIGH);
   digitalWrite(DIR_B, LOW);
   analogWrite(PWM, spd);
 }
+
 void reverse(int spd)
 {
   digitalWrite(DIR_A, LOW);
   digitalWrite(DIR_B, HIGH);
   analogWrite(PWM, spd);
-
-
-
-
-
-
 }
